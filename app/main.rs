e0af156cece91a6591892aa02fabb672d4c65ee6
siavava <amittaijoel@outlook.com>
2023-07-07 01:46:20 -0400

    C11

--- app/main.rs
+++ app/main.rs
@@ -1,25 +1,6 @@
 use ttt::game::{Game, Move};
 
 fn main() {
-  println!("Hello, world!");
-
-  let mut game = Game::new(3);
-  //game.print();
-
-  println!("{}", game);
-
-  let moves = game.get_moves();
-  println!("{:?}", moves);
-
-  game.make_move((0, 0));
-
-  game.make_move((1, 1));
-
-  game.make_move((0, 1));
-
-  println!("{}", game);
-
-  println!("{:?}", game.get_moves());
 
   let mut game = Game::new(4);
 
